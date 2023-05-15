9118770953dd1f56db5ccd69cc3b3ef5c2814251
Yaraly10 <129077242+Yaraly10@users.noreply.github.com>
2023-05-15 09:26:55 -0500

    main

---
+++ Javascript.js
@@ -0,0 +1,7 @@
+
+//Preguntar al usuario en que año nacio. 
+//Deberan calcular su edad en base al año que nacio.
+let nacimiento = prompt("Introduzca el año que nacio");
+document.write(nacimiento); 
+let edad = prompt("edad");
+document.write(edad);
